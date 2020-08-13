aa5c1295b92876ac7cc90d986a0747fbbabfccbd
Diane Tobit <dianetobit@Dianes-MacBook-Pro.local>
2020-08-13 03:14:09 -0700

    switch to icon for logout

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,18 +1,15 @@
-import { NavigationContainer, DefaultTheme, DarkTheme, useNavigation } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { ColorSchemeName } from 'react-native';
 
-import NotFoundScreen from '../screens/NotFoundScreen';
-import LoadingScreen from '../screens/LoadingScreen';
-import LoginScreen from '../screens/LoginScreen';
 import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import AuthNavigator from './AuthNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 import { User } from '@firebase/auth-types'
-import { Text, TouchableOpacity } from 'react-native';
-import { loginUser, logoutUser } from "../api/auth-api";
+import { Icon } from 'react-native-elements';
+import { logoutUser } from "../api/auth-api";
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -40,11 +37,12 @@ function RootNavigator({authUser, setUser}: {authUser : User | null, setUser: Fu
           options={{ 
             headerShown: true,
             headerRight: () => (
-              <TouchableOpacity
+              <Icon
+                name='sign-out'
+                type='font-awesome'
                 onPress={() => {setUser(null); logoutUser()}}
               >
-                <Text>Logout</Text>
-              </TouchableOpacity>
+              </Icon>
             ),
         }}
         />
